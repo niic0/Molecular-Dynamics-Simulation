7978b31d9178447da65abcc35a83db4b7947597b
Nicolas Fond-Massany <nicolas.fond@ens.uvsq.fr>
2025-02-16 17:02:58 +0100

    Adding Verlet list

--- include/simulation.h
+++ include/simulation.h
@@ -9,8 +9,21 @@ Particles molecular_simulation(SimulationParameters params);
 void compute_velocity_verlet(Particles &p, u32 N, f64 dt, f64 mass,
                              std::vector<Vec3>& periodic_images,
                              f64 R_cut_squared, u32 N_sym);
+void compute_velocity_verlet_neighbor(Particles &p, u32 N, f64 dt, f64 mass,
+                                      std::vector<Vec3> &periodic_images,
+                                      f64 R_cut_squared, u32 N_sym,
+                                      std::vector<std::vector<u32>> neighbor);
 
-void compute_kinetic_energy(Particles& p, f64& kinetic_energy, u32 N, f64 mass);
+void compute_forces_pbc_neighbor(Particles &p,
+                                 std::vector<std::vector<u32>> &neighbor, u32 N,
+                                 std::vector<Vec3> &periodic_images,
+                                 f64 R_cut_squared, u32 N_sym);
+
+void build_verlet_lists(Particles &p, std::vector<std::vector<u32>> &neighbor,
+                        u32 N, std::vector<Vec3> &periodic_images,
+                        f64 R_cut_squared, u32 N_sym, u32 n_max_neighbor);
+
+void compute_kinetic_energy(Particles &p, f64 &kinetic_energy, u32 N, f64 mass);
 void compute_kinetic_temperature_energy(f64& kinetic_energy, u32 N, f64& temperature);
 void compute_sum_forces(Particles& p, f64& totalFx, f64& totalFy, f64& totalFz, u32 N);
 void compute_potential_energy(Particles& p, f64& U_total, std::vector<Vec3>& periodic_images, u32 N, f64 R_cut_squared, u32 N_sym);
--- src/simulation.cpp
+++ src/simulation.cpp
@@ -55,8 +55,13 @@ Particles molecular_simulation(SimulationParameters params) {
   f64 R_cut_squared = params.cutoff * params.cutoff;
   u32 N_sym = params.N_sym;
 
-  // Variables to store kinetic energy and temperature during the simulation
+  // Current kinetic energy and temperature variables
   f64 kinetic_energy, current_temperature;
+
+  // Verlet list variables
+  f64 density = N / (box_size * box_size * box_size);
+  u32 n_max_neighbor = static_cast<u32>(density * 4 * M_PI * params.cutoff * params.cutoff * params.cutoff) * 2;
+  std::vector<std::vector<u32>> neighbor(N, std::vector<u32>(2 * n_max_neighbor + 1));
 
   // Create the output csv and pdb file
   create_csv_file(csv_output_filename);
@@ -72,6 +77,10 @@ Particles molecular_simulation(SimulationParameters params) {
   // Calibrate momenta to match the target temperature
   kinetic_calibration(p, target_temperature, N, mass);
 
+  // Build the verlet (neighbors) list, this list is updated each
+  // "relaxation_time"
+  build_verlet_lists(p, neighbor, N, periodic_images, R_cut_squared, N_sym, n_max_neighbor);
+
   for (u32 i = 0; i < params.steps; i++) {
     // Compute kinetic energy and temperature
     compute_kinetic_energy(p, kinetic_energy, N, mass);
@@ -82,13 +91,16 @@ Particles molecular_simulation(SimulationParameters params) {
     append_pdb(pdb_output_filename, p, i, box_size, N);
 
     // Perform one step of the Velocity Verlet algorithm
-    compute_velocity_verlet(p, N, dt, mass, periodic_images, R_cut_squared, N_sym);
+    compute_velocity_verlet_neighbor(p, N, dt, mass, periodic_images, R_cut_squared,
+                            N_sym, neighbor);
 
     // Apply the Berendsen thermostat periodically to maintain the target
     // temperature
     if (i % relaxation_time == 0) {
       apply_berendsen_thermostat(p, current_temperature, target_temperature,
                                  params.num_particles);
+      build_verlet_lists(p, neighbor, N, periodic_images, R_cut_squared, N_sym,
+                         n_max_neighbor);
     }
 
     // Ensure particles remain within the simulation box using periodic boundary
@@ -108,24 +120,35 @@ Particles molecular_simulation(SimulationParameters params) {
 }
 
 /**
- * @brief Computes the forces between particles using the Lennard-Jones potential with periodic boundary conditions (PBC).
+ * @brief Computes the forces between particles using the Lennard-Jones
+ * potential with periodic boundary conditions (PBC).
  *
- * This function calculates the forces between all pairs of particles in the system, taking into account
- * periodic boundary conditions. The forces are computed using the Lennard-Jones potential, and interactions
- * beyond the cutoff distance (`R_cut_squared`) are ignored. The forces are updated in place for each particle.
+ * This function calculates the forces between all pairs of particles in the
+ * system, taking into account periodic boundary conditions. The forces are
+ * computed using the Lennard-Jones potential, and interactions beyond the
+ * cutoff distance (`R_cut_squared`) are ignored. The forces are updated in
+ * place for each particle.
  *
- * @param p Reference to the Particles object containing the particle positions and forces.
+ * @param p Reference to the Particles object containing the particle positions
+ * and forces.
  * @param N Number of particles in the system.
- * @param periodic_images Vector of periodic image translations to apply for PBC.
+ * @param periodic_images Vector of periodic image translations to apply for
+ * PBC.
  * @param R_cut_squared Squared cutoff distance for the Lennard-Jones potential.
  *
- * @note This function modifies the forces (`fx`, `fy`, `fz`) of the particles in place.
+ * @note This function modifies the forces (`fx`, `fy`, `fz`) of the particles
+ * in place.
  * @note The Lennard-Jones potential is computed as:
  *       U(r) = -48 * epsilon * [(r_star/r)^14 - (r_star/r)^8]
- *       where sigma is the distance at which the potential is zero, and epsilon is the depth of the potential well.
- * @note The cutoff distance is used to ignore interactions beyond a certain range for efficiency.
+ *       where sigma is the distance at which the potential is zero, and epsilon
+ * is the depth of the potential well.
+ * @note The cutoff distance is used to ignore interactions beyond a certain
+ * range for efficiency.
+ * @warning This function is not used because it doesn't use verlet list which
+ * is faster.
  */
-void compute_forces_pbc(Particles &p, u32 N, std::vector<Vec3>& periodic_images, f64 R_cut_squared, u32 N_sym) {
+void compute_forces_pbc(Particles &p, u32 N, std::vector<Vec3> &periodic_images,
+                        f64 R_cut_squared, u32 N_sym) {
   for (u32 i_sym = 0; i_sym < N_sym; i_sym++) {
     for (u32 i = 0; i < N; i++) {
       for (u32 j = i + 1; j < N; j++) {
@@ -139,9 +162,6 @@ void compute_forces_pbc(Particles &p, u32 N, std::vector<Vec3>& periodic_images,
         f64 dy = p.y[i] - yj_loc;
         f64 dz = p.z[i] - zj_loc;
         f64 rij_squared = dx * dx + dy * dy + dz * dz;
-
-        // Prevent Division by 0
-        assert(rij_squared != 0 && "Division by 0");
         f64 rij_inv_squared = 1 / rij_squared;
 
         if (rij_squared < R_cut_squared) {
@@ -150,7 +170,7 @@ void compute_forces_pbc(Particles &p, u32 N, std::vector<Vec3>& periodic_images,
           f64 potential2 = potential * potential;            // (r*^2 / rij^2)^2
           f64 potential3 = potential2 * potential;           // (r*^2 / rij^2)^3
           f64 potential4 = potential2 * potential2;          // (r*^2 / rij^2)^4
-          f64 potential7 = potential4 * potential3;          // (r*^3 / rij^4)^7
+          f64 potential7 = potential4 * potential3;          // (r*^2 / rij^2)^7
 
           // Compute forces using the gradient of the Lennard-Jones potential
           f64 grad_inner = potential7 - potential4;
@@ -196,6 +216,8 @@ void compute_forces_pbc(Particles &p, u32 N, std::vector<Vec3>& periodic_images,
  * interactions at the new positions.
  * @note The forces are reset to zero before recalculating them to avoid
  * accumulating contributions from previous steps.
+ * @warning This function is not use because it doesn't uses verlet lists so it
+ * is less optimize
  */
 void compute_velocity_verlet(Particles &p, u32 N, f64 dt, f64 mass,
                              std::vector<Vec3>& periodic_images,
@@ -224,7 +246,192 @@ void compute_velocity_verlet(Particles &p, u32 N, f64 dt, f64 mass,
   }
 
   // Compute forces with Lennard-Jones potential at the new positions (t + dt)
+  // without verlet list
   compute_forces_pbc(p, N, periodic_images, R_cut_squared, N_sym);
+
+  // Final update of momentum (p = p - 0.5 * F * dt)
+  for(u32 i = 0; i<N; i++) {
+    p.mx[i] -= 0.5 * p.fx[i] * force_conversion * dt;
+    p.my[i] -= 0.5 * p.fy[i] * force_conversion * dt;
+    p.mz[i] -= 0.5 * p.fz[i] * force_conversion * dt;
+  }
+}
+
+/**
+ * @brief Builds the Verlet neighbor lists for all particles.
+ *
+ * This function constructs the Verlet neighbor lists for each particle, storing
+ * the indices of neighboring particles and their periodic images within the
+ * cutoff distance.
+ *
+ * @param p Reference to the Particles object containing the particle positions.
+ * @param neighbor Reference to the 2D array storing the neighbor lists.
+ * @param N Number of particles in the system.
+ * @param periodic_images Vector of periodic image translations to apply for
+ * PBC.
+ * @param R_cut_squared Squared cutoff distance for the Lennard-Jones potential.
+ * @param N_sym Number of periodic images to consider.
+ * @param n_max_neighbor Maximum number of neighbors per particle.
+ */
+void build_verlet_lists(Particles &p, std::vector<std::vector<u32>> &neighbor,
+                        u32 N, std::vector<Vec3> &periodic_images,
+                        f64 R_cut_squared, u32 N_sym, u32 n_max_neighbor) {
+  for (u32 i = 0; i < N; i++) {
+    u32 ni = 0; // Number of neighbors for particle i
+    for (u32 j = i+1; j < N; j++) {
+      if (i == j)
+        continue; // Skip self-interaction
+      for (u32 i_sym = 0; i_sym < N_sym; i_sym++) {
+        // Translating particle j position with periodic boundary conditions
+        f64 xj_loc = p.x[j] + periodic_images[i_sym].x;
+        f64 yj_loc = p.y[j] + periodic_images[i_sym].y;
+        f64 zj_loc = p.z[j] + periodic_images[i_sym].z;
+
+        // Compute squared distance between particles i and j
+        f64 dx = p.x[i] - xj_loc;
+        f64 dy = p.y[i] - yj_loc;
+        f64 dz = p.z[i] - zj_loc;
+        f64 rij_squared = dx * dx + dy * dy + dz * dz;
+
+        // Store the neighbor index and periodic image if under cutoff
+        if (rij_squared < R_cut_squared) {
+          u32 mi = 2 * ni + 1;
+          neighbor[i][mi] = j;         // Index of neighbor j
+          neighbor[i][mi+1] = i_sym;   // Periodic image index
+          ni++;
+        }
+      }
+    }
+
+    // Store the number of neighbors for particle i in [i,0]
+    neighbor[i][0] = ni;
+  }
+}
+
+
+/**
+ * @brief Computes the forces between particles using the Lennard-Jones potential
+ * with periodic boundary conditions (PBC) and Verlet neighbor lists.
+ *
+ * This function calculates the forces between particles using precomputed Verlet
+ * neighbor lists, which store the indices of neighboring particles within the
+ * cutoff distance. This reduces the computational cost compared to the naive O(N^2)
+ * approach.
+ *
+ * @param p Reference to the Particles object containing the particle positions
+ * and forces.
+ * @param neighbor Reference to the 2D array storing the Verlet neighbor lists.
+ * @param N Number of particles in the system.
+ * @param periodic_images Vector of periodic image translations to apply for PBC.
+ * @param R_cut_squared Squared cutoff distance for the Lennard-Jones potential.
+ * @param N_sym Number of periodic images to consider.
+ */
+void compute_forces_pbc_neighbor(Particles &p, std::vector<std::vector<u32>> &neighbor, u32 N,
+                        std::vector<Vec3> &periodic_images, f64 R_cut_squared, u32 N_sym) {
+  // Loop over all particles
+  for (u32 i = 0; i < N; i++) {
+    // Get the number of neighbors for particle i
+    u32 ni = neighbor[i][0];
+
+    // Loop over all neighbors of particle i
+    for (u32 k = 0; k < ni; k++) {
+      // Get the index of the neighbor particle j and its periodic image
+      u32 j = neighbor[i][2 * k + 1];     // Index of neighbor j
+      u32 i_sym = neighbor[i][2 * k + 2]; // Periodic image index
+
+      // Translating particle j position with periodic boundary conditions
+      f64 xj_loc = p.x[j] + periodic_images[i_sym].x;
+      f64 yj_loc = p.y[j] + periodic_images[i_sym].y;
+      f64 zj_loc = p.z[j] + periodic_images[i_sym].z;
+
+      // Compute squared distance between particles i and j
+      f64 dx = p.x[i] - xj_loc;
+      f64 dy = p.y[i] - yj_loc;
+      f64 dz = p.z[i] - zj_loc;
+      f64 rij_squared = dx * dx + dy * dy + dz * dz;
+
+      // Prevent division by zero
+      //assert(rij_squared != 0 && "Division by 0");
+      f64 rij_inv_squared = 1.0 / rij_squared;
+
+      // Compute repulsion and dispersion terms of the Lennard-Jones potential
+      f64 potential = r_star_squared * rij_inv_squared; // (r*^2 / rij^2)
+      f64 potential2 = potential * potential;           // (r*^2 / rij^2)^2
+      f64 potential3 = potential2 * potential;          // (r*^2 / rij^2)^3
+      f64 potential4 = potential2 * potential2;         // (r*^2 / rij^2)^4
+      f64 potential7 = potential4 * potential3;         // (r*^3 / rij^4)^7
+
+      // Compute forces using the gradient of the Lennard-Jones potential
+      f64 grad_inner = potential7 - potential4;
+      f64 common_factor = -48.0 * epsilon * grad_inner;
+      f64 fx = common_factor * dx;
+      f64 fy = common_factor * dy;
+      f64 fz = common_factor * dz;
+
+      // Update forces on particles i and j (Newton's third law)
+      p.fx[i] += fx;
+      p.fy[i] += fy;
+      p.fz[i] += fz;
+      p.fx[j] -= fx;
+      p.fy[j] -= fy;
+      p.fz[j] -= fz;
+    }
+  }
+}
+
+/**
+ * @brief Performs a single time step of the Velocity Verlet integration
+ * algorithm.
+ *
+ * This function updates the positions and momentum of all particles in the
+ * system using the Velocity Verlet algorithm. The algorithm consists of three
+ * main steps:
+ * 1. Intermediate update of momentum using the current forces.
+ * 2. Update of positions using the intermediate momentum.
+ * 3. Recalculation of forces at the new positions and final update of momentum.
+ *
+ * @param p Reference to the Particles object containing the particle positions,
+ * momentum, and forces.
+ * @param N Number of particles in the system.
+ * @param dt Time step for the integration.
+ * @param mass Mass of the particles (assumed to be the same for all particles).
+ * @param periodic_images Vector of periodic image translations to apply for
+ * periodic boundary conditions (PBC).
+ * @param R_cut_squared Squared cutoff distance for the Lennard-Jones potential.
+ *
+ * @note The forces are recalculated after updating the positions to account for
+ * interactions at the new positions.
+ * @note The forces are reset to zero before recalculating them to avoid
+ * accumulating contributions from previous steps.
+ */
+void compute_velocity_verlet_neighbor(Particles &p, u32 N, f64 dt, f64 mass,
+                             std::vector<Vec3>& periodic_images,
+                             f64 R_cut_squared, u32 N_sym, std::vector<std::vector<u32>> neighbor) {
+  const f64 mass_inv = 1 / mass;
+
+  // Intermediate update of momentum (p = p - 0.5 * F * dt)
+  for (u32 i = 0; i<N; i++) {
+    p.mx[i] -= 0.5 * p.fx[i] * force_conversion * dt;
+    p.my[i] -= 0.5 * p.fy[i] * force_conversion * dt;
+    p.mz[i] -= 0.5 * p.fz[i] * force_conversion * dt;
+  }
+
+  // Update position with the formula (x = x + (p/m) * dt)
+  for(u32 i = 0; i<N; i++) {
+    p.x[i] += p.mx[i] * mass_inv * dt;
+    p.y[i] += p.my[i] * mass_inv * dt;
+    p.z[i] += p.mz[i] * mass_inv * dt;
+  }
+
+  // Reset forces to zero before computing new forces
+  for (u32 i = 0; i < N; i++) {
+    p.fx[i] = 0.0;
+    p.fy[i] = 0.0;
+    p.fz[i] = 0.0;
+  }
+
+  // Compute forces with Lennard-Jones potential at the new positions (t + dt)
+  compute_forces_pbc_neighbor(p, neighbor, N, periodic_images, R_cut_squared, N_sym);
 
   // Final update of momentum (p = p - 0.5 * F * dt)
   for(u32 i = 0; i<N; i++) {
@@ -281,7 +488,7 @@ void compute_potential_energy(Particles &p, f64 &U_total,
         f64 rij_squared = dx * dx + dy * dy + dz * dz;
 
         // Prevent Division by 0
-        // assert(rij_squared != 0 && "Division by 0");
+        assert(rij_squared != 0 && "Division by 0");
         f64 rij_inv_squared = 1.0 / rij_squared;
 
         if (rij_squared < R_cut_squared) {
